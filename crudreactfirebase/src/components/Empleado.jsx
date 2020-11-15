c351e4cf8fea350d97120be27b3c3bdf60337919
Alejandro Barrios <oscarbarrios17@gmail.com>
2020-11-14 20:12:05 -0600

    Salario menor/mayor y detalle individual

--- crudreactfirebase/src/components/Empleado.jsx
+++ crudreactfirebase/src/components/Empleado.jsx
@@ -3,12 +3,16 @@ import EmpleadosForm from "./EmpleadosForm";
 
 import { db } from "../Firebase";
 import { toast } from "react-toastify";
+import MenorSalario from "./MenorSalario";
+import MayorSalario from "./MayorSalario";
 
 const Empleados = () => {
   const [Empleados, setEmpleados] = useState([]);
+  const [Empleado, setEmpleado] = useState([]);
   const [currentId, setCurrentId] = useState("");
 
   const getEmpleados = async () => {
+    //db.collection('Empleados').orderBy('salarioN', 'asc').limit(1).onSnapshot((querySnapshot) => {
     db.collection("Empleados").onSnapshot((querySnapshot) => {
       const docs = [];
       querySnapshot.forEach((doc) => {
@@ -26,6 +30,26 @@ const Empleados = () => {
         //autoClose: 2000
       });
     }
+  };
+
+  const detalleEmpleado = async (id) => {
+      db.collection("Empleados").doc(id).get()
+      .then(doc => {
+        if (!doc.exists) {
+          console.log('No such document!');
+        } else {
+          const docs = [];
+          docs.push({ ...doc.data(), id: doc.id });
+          console.log('Document data:', doc.data());
+          setEmpleado(docs);
+        }
+      })
+        
+
+      /*toast("Se elimino un empleado", {
+        type: "error",
+        //autoClose: 2000
+      });*/
   };
 
   useEffect(() => {
@@ -72,11 +96,50 @@ const Empleados = () => {
           });
         }
       } else {
-        await db.collection("Empleados").doc(currentId).update(EmpleadoObject);
+        /*await db.collection("Empleados").doc(currentId).update(EmpleadoObject);
         toast("Se actualizo un Empleado", {
           type: "info",
         });
-        setCurrentId("");
+        setCurrentId("");*/
+        if (EmpleadoObject.hTrabajo > 0 && EmpleadoObject.hTrabajo <=160){
+          EmpleadoObject.salarioL = EmpleadoObject.hTrabajo * 9.75;
+          EmpleadoObject.AFP = EmpleadoObject.salarioL * 0.0688;
+          EmpleadoObject.ISSS = EmpleadoObject.salarioL * 0.0525;
+          EmpleadoObject.renta = EmpleadoObject.salarioL * 0.10;
+          EmpleadoObject.salarioN = EmpleadoObject.salarioL - EmpleadoObject.AFP- EmpleadoObject.ISSS - EmpleadoObject.renta;
+          await db.collection("Empleados").doc(currentId).update(EmpleadoObject);
+          toast("Se actualizo un Empleado", {
+            type: "info",
+          });
+          setCurrentId("");
+        } else if (EmpleadoObject.hTrabajo > 160 && EmpleadoObject.hTrabajo <=200){
+          EmpleadoObject.salarioL = (160* 9.75) + ((EmpleadoObject.hTrabajo-160)*11.50);
+          EmpleadoObject.AFP = EmpleadoObject.salarioL * 0.0688;
+          EmpleadoObject.ISSS = EmpleadoObject.salarioL * 0.0525;
+          EmpleadoObject.renta = EmpleadoObject.salarioL * 0.10;
+          EmpleadoObject.salarioN = EmpleadoObject.salarioL - EmpleadoObject.AFP- EmpleadoObject.ISSS - EmpleadoObject.renta;
+          await db.collection("Empleados").doc(currentId).update(EmpleadoObject);
+          toast("Se actualizo un Empleado", {
+            type: "info",
+          });
+          setCurrentId("");
+        } else if (EmpleadoObject.hTrabajo > 200 && EmpleadoObject.hTrabajo <=250){
+          EmpleadoObject.salarioL = (160* 9.75) + (40*11.50) + ((EmpleadoObject.hTrabajo-200)*12.50);
+          EmpleadoObject.AFP = EmpleadoObject.salarioL * 0.0688;
+          EmpleadoObject.ISSS = EmpleadoObject.salarioL * 0.0525;
+          EmpleadoObject.renta = EmpleadoObject.salarioL * 0.10;
+          EmpleadoObject.salarioN = EmpleadoObject.salarioL - EmpleadoObject.AFP- EmpleadoObject.ISSS - EmpleadoObject.renta;
+          await db.collection("Empleados").doc(currentId).update(EmpleadoObject);
+          toast("Se actualizo un Empleado", {
+            type: "info",
+          });
+          setCurrentId("");
+        }else {
+          toast("Debe ingresar una cantidad valida de horas", {
+            type: "error",
+            //autoClose: 2000
+          });
+        }
       }
     } catch (error) {
       console.error(error);
@@ -88,10 +151,30 @@ const Empleados = () => {
       <div className="col-md-4 p-2">
         <h2>Agregar Empleado</h2>
         <EmpleadosForm {...{ addOrEditEmpleado, currentId, Empleados }} />
+        <br/>
+        <div className="card card-body border-primary">
+        <h4>Detalle salario</h4>
+        {Empleado.map((Empleadod) => (
+                <tr key={Empleadod.id}>
+                <p>Codigo: {Empleadod.codigo}</p>
+                  <p>Nombre: {Empleadod.nombre}</p>
+                  <p>Apellido: {Empleadod.apellido}</p>
+                  <p>Horas:{Empleadod.hTrabajo}</p>
+                  <p>Salario Liquido: ${Empleadod.salarioL.toFixed(2)}</p>
+                  <p>AFP: ${Empleadod.AFP.toFixed(2)}</p>
+                  <p>ISSS: ${Empleadod.ISSS.toFixed(2)}</p>
+                  <p>Renta: ${Empleadod.renta.toFixed(2)}</p>
+                  <p>Salario Neto: ${Empleadod.salarioN.toFixed(2)}</p>
+                </tr>
+              ))}
+        </div>
       </div>
 
       <div className="col-md-8 p-2">
+        
         <div class="container">
+        <MayorSalario/>
+        <MenorSalario/>
           <h2>Lista Empleados</h2>
           <table class="table table-hover">
             <thead>
@@ -119,7 +202,7 @@ const Empleados = () => {
                     <button className="btn btn-danger" onClick={() => onDeleteEmpleado(Empleado.id)}>Eliminar</button>
                     &nbsp;
                     &nbsp;
-                    <button className="btn btn-success" >Calcular el pago</button>
+                    <button className="btn btn-success" onClick={() => detalleEmpleado(Empleado.id)}>Detalle</button>
                   </td>
                 </tr>
               ))}
---
+++ crudreactfirebase/src/components/MenorSalario.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import Empleadoss from "./Empleado";
+import { db } from "../Firebase";
+
+const MenorSalario = () => {
+  const [Empleados, setEmpleados] = useState([]);
+  const [currentId, setCurrentId] = useState("");
+
+  const getEmpleados = async () => {
+    db.collection('Empleados').orderBy('salarioN', 'asc').limit(1).onSnapshot((querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setEmpleados(docs);
+    });
+  };
+
+  useEffect(() => {
+    getEmpleados();
+  }, []);
+
+
+  return (
+    <>
+
+
+          <h5>Empleado con menor salario</h5>
+          <table class="table table-hover">
+            <thead>
+              <tr>
+              <th>Codigo</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Horas </th>
+                <th>Salario</th>
+                <th>AFP</th>
+                <th>ISSS</th>
+                <th>Renta</th>
+                <th>Salario Neto</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Empleados.map((Empleado) => (
+                <tr key={Empleado.id}>
+                <td>{Empleado.codigo}</td>
+                  <td>{Empleado.nombre}</td>
+                  <td>{Empleado.apellido}</td>
+                  <td>{Empleado.hTrabajo}</td>
+                  <td>${Empleado.salarioL.toFixed(2)}</td>
+                  <td>${Empleado.AFP.toFixed(2)}</td>
+                  <td>${Empleado.ISSS.toFixed(2)}</td>
+                  <td>${Empleado.renta.toFixed(2)}</td>
+                  <td>${Empleado.salarioN.toFixed(2)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+    </>
+  );
+};
+export default MenorSalario;
